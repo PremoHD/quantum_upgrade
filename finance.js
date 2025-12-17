8766384379357b4547795d60573d56a9a8e0efdf
PremoHD <rd86c2s5qh@privaterelay.appleid.com>
2025-12-17 17:31:01 -0500

    Create finance.js

---
+++ finance.js
@@ -0,0 +1,7 @@
+const API="http://127.0.0.1:5000";
+
+async function runFinance(){
+  let res=await fetch(`${API}/finance/portfolio`);
+  let data=await res.json();
+  document.getElementById("finance-output").innerText=JSON.stringify(data,null,2);
+}
