61aab27862c0e4ea42f78b79bb00773341454d4a
PremoHD <rd86c2s5qh@privaterelay.appleid.com>
2025-12-20 14:38:08 -0500

    Update finance.js

--- finance.js
+++ finance.js
@@ -1,3 +1,6 @@
+const API = window.location.hostname.includes("github")
+  ? "https://YOUR-CODESPACE-URL"
+  : "http://127.0.0.1:5000";
 const API="http://127.0.0.1:5000";
 
 async function runFinance(){
