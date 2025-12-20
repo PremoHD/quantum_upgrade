ccd05c34e8ed52eec05cafde8b5ce3b635cdf650
PremoHD <rd86c2s5qh@privaterelay.appleid.com>
2025-12-20 14:48:56 -0500

    Update legal.js

--- legal.js
+++ legal.js
@@ -1,5 +1,5 @@
 const API = window.location.hostname.includes("github")
-  ? "https://YOUR-CODESPACE-URL"
+  ? "https://upgraded-sniffle-97rq4v4vp5p5c69p-8080.app.github.dev"
   : "http://127.0.0.1:5000";
 async function runLegal(){
   document.getElementById("legal-output").innerText=JSON.stringify(await (await fetch(`${API}/legal/workflow`)).json(),null,2);
