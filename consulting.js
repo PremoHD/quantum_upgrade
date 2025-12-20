a1a2b218deeac0b9025ed8a90bd5ea9d39734444
PremoHD <rd86c2s5qh@privaterelay.appleid.com>
2025-12-20 14:36:04 -0500

    Update consulting.js

--- consulting.js
+++ consulting.js
@@ -1,3 +1,6 @@
+const API = window.location.hostname.includes("github")
+  ? "https://YOUR-CODESPACE-URL"
+  : "http://127.0.0.1:5000";
 async function runConsult(){
   document.getElementById("consulting-output").innerText=JSON.stringify(await (await fetch(`${API}/consulting/schedule`)).json(),null,2);
 }
