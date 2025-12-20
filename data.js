bbb17c47ffc5f73ab6a1e25a5c0cb7ded60171d8
PremoHD <rd86c2s5qh@privaterelay.appleid.com>
2025-12-20 14:36:28 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
+const API = window.location.hostname.includes("github")
+  ? "https://YOUR-CODESPACE-URL"
+  : "http://127.0.0.1:5000";
 async function runData(){
   document.getElementById("data-output").innerText=JSON.stringify(await (await fetch(`${API}/data/simulate`)).json(),null,2);
 }
