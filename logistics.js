435a61927bb4a0b71e5991a575ab24a9bd0cb61e
PremoHD <rd86c2s5qh@privaterelay.appleid.com>
2025-12-20 14:37:26 -0500

    Update logistics.js

--- logistics.js
+++ logistics.js
@@ -1,3 +1,6 @@
+const API = window.location.hostname.includes("github")
+  ? "https://YOUR-CODESPACE-URL"
+  : "http://127.0.0.1:5000";
 async function runLogistics(){
   document.getElementById("logistics-output").innerText=JSON.stringify(await (await fetch(`${API}/logistics/routes`)).json(),null,2);
 }
